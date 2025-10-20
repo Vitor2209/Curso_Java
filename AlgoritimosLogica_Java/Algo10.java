8f11a21bae4cb0ced33d8ef91fa5ba9d5f081bc2
Vitor Dutra Melo <vitordutra1125@gmail.com>
2025-10-20 21:35:27 +0100

    progress in Java studies 🚀

--- AlgoritimosLogica_Java/Algo10.java
+++ AlgoritimosLogica_Java/Algo10.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
 public class Algo10 {
     public static void main(String[] args) {
-        
+        //Variaveis 
+        float altura, peso_ideal;
+        char sexo;
+        Scanner teclado = new Scanner(System.in);
+
+        //Entradas 
+        System.out.println("Informe sua altura: ");
+        altura = teclado.nextFloat();
+
+        System.out.println("Informe o sexo m/f: ");
+        sexo = teclado.next().charAt(0); //"banana"
+
+        //Processamento
+        if(sexo == 'm') {
+            peso_ideal = (float) (72.7 * altura) - 58;
+            System.out.println("Seu peso ideal e " + peso_ideal);
+        }
+        if(sexo == 'f') {
+            peso_ideal = (float) (62.1 * altura) - (float)44.7;
+            System.out.println("Seu peso ideal e " + peso_ideal);
+
+        }
+        if(sexo != 'm' && sexo != 'f') {
+            System.out.println("Sexo nao reconhecido.");
+            peso_ideal = 0;
+        }
+          teclado.close();
     }
+
+        
 }
